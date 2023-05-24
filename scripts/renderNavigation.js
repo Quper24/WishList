b6b76687d19e58864004a99051b27e4f5745a50e
Maksim Leskin <quper2405@gmail.com>
2023-05-24 17:05:14 +0300

    render hero

---
+++ scripts/renderNavigation.js
@@ -0,0 +1,31 @@
+import { createElement } from './helper.js';
+
+const nav = document.querySelector('.nav');
+// const burger = createBurgerMenu(nav);
+
+export const renderNavigation = () => {
+  nav.textContent = '';
+
+  // <button class="nav__btn btn">Зарегистрироваться</button>
+  // <button class="nav__btn btn">Войти</button>
+
+  const buttonSignUp = createElement('button', {
+    className: 'nav__btn btn',
+    textContent: 'Зарегистрироваться'
+  });
+
+  buttonSignUp.addEventListener('click', () => {
+    console.log('Зарегистрироваться')
+  })
+
+  const buttonLogin = createElement('button', {
+    className: 'nav__btn btn',
+    textContent: 'Войти'
+  });
+
+  buttonLogin.addEventListener('click', () => {
+    console.log('Войти')
+  })
+
+  nav.append(buttonSignUp, buttonLogin)
+}
