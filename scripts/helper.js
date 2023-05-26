bae205611cb88d223dafdd924f49f8b9db98b78e
Maksim Leskin <quper2405@gmail.com>
2023-05-26 18:55:24 +0300

    renderCreateEditProfile

--- scripts/helper.js
+++ scripts/helper.js
@@ -20,3 +20,71 @@ export const pluralizeYears = (age) => {
     }
   }
 };
+
+export const handleImageFileSelection = (input, image) => {
+  // !todo
+};
+
+export const createSelectDate = (
+  selectDay,
+  selectMonth,
+  selectYear,
+  birhdate,
+) => {
+  for (let day = 0; day <= 31; day++) {
+    const option = document.createElement('option');
+    option.value = day ? day : '';
+    option.text = day ? day : '';
+    selectDay.append(option);
+  }
+
+  const months = [
+    '',
+    'Янв',
+    'Фев',
+    'Мар',
+    'Апр',
+    'Май',
+    'Июн',
+    'Июл',
+    'Авг',
+    'Сен',
+    'Отк',
+    'Ноя',
+    'Дек',
+  ];
+
+  for (let i = 0; i < months.length; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.text = months[i];
+    selectMonth.append(option);
+  }
+
+  const currentYear = new Date().getFullYear();
+
+  const optionYear = document.createElement('option');
+  optionYear.value = '';
+  optionYear.text = '';
+  selectYear.append(optionYear);
+
+  for (let year = currentYear; year >= currentYear - 100; year--) {
+    const option = document.createElement('option');
+    option.value = year;
+    option.text = year;
+    selectYear.append(option);
+  }
+
+  if (birhdate) {
+    const [day, month, year] = birhdate.split('/');
+    selectDay.value = day;
+    selectMonth.value = month;
+    selectYear.value = year;
+  }
+
+  [selectDay, selectMonth, selectYear].forEach(dateSelect => {
+    dateSelect.addEventListener('change', ({currentTarget}) => {
+      currentTarget.blur();
+    })
+  })
+};
